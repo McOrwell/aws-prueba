c2a8ba61c0cd85280a0bef8ef37ac0ae4563174d
McOrwell <setar-bosnai@hotmail.com>
2023-02-19 17:40:44 -0600

    Se cambió el puerto del servidor del 3000 al 80 para ver qué pasa en aws

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ require('./database');
 require('./passport/local-auth');
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'))
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
